b724f3549a9a73f2f16a3b7950d76b6537083f91
staf-teamdubai <54719389+staf-teamdubai@users.noreply.github.com>
2019-09-03 14:36:33 +0100

    Add files via upload
    All codes

---
+++ contact.php
@@ -0,0 +1,38 @@
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="ngo.css" rel="stylesheet" type="text/css">
+</head>
+
+
+
+</h4>
+</div>
+	<form>
+<div class="contain">
+	<h2>Send E-mail</h2>
+	<div class="TextBox">
+	<input type="text" placeholder="Full Name" name="name" required />
+		</div>
+	<div class="TextBox">
+	<input type="text" placeholder="Your Email" name="mail" required />
+		</div>
+	<div class="TextBox">
+		<input type="text" placeholder="Subject" name="subject" required />
+		</div>
+	<div class="TextBox">
+	<textarea name="message" placeholder="Message" required></textarea>
+		</div>
+		<button type="submit" class="but" name="submit">submit</button>
+		</div>
+		</form>
+    
+
+
+</div>
+</body>
+</html>
